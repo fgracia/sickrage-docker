6b5facc7a0aba1450abdffc47f58b306675f07cd
Frédéric GRACIA <fgracia@sqli.com>
2017-02-07 11:32:53 +0100

    Better init, you can now have only config.ini

--- startup.sh
+++ startup.sh
@@ -14,13 +14,13 @@ then
 fi
 
 if [ -f /data/failed.db ]
-then	
+then
 	rm -f /opt/SickRage/failed.db
 	ln -s /data/failed.db /opt/SickRage/failed.db
 fi
 
 if [ -f /data/sickbeard.db ]
-then	
+then
 	rm -f /opt/SickRage/sickbeard.db
 	ln -s /data/sickbeard.db /opt/SickRage/sickbeard.db
 fi
@@ -29,33 +29,48 @@ if [ -d /data/cache ]
 then
 	rm -rf /opt/SickRage/cache
 	ln -s /data/cache /opt/SickRage/cache
-fi	
+fi
 
 if [ -d /data/Logs ]
 then
 	rm -rf /opt/SickRage/Logs
 	ln -s /data/Logs /opt/SickRage/Logs
-fi	
+fi
 
 
 # If no config is present, we create it
-if [ ! -f /data/sickbeard.db ] && [ ! -f /data/config.ini ]
+if [ ! -f /data/sickbeard.db ]
+then
+	touch /data/sickbeard.db
+	ln -s /data/sickbeard.db /opt/SickRage/sickbeard.db
+fi
+
+if [ ! -f /data/cache.db ]
 then
 	touch /data/cache.db
 	ln -s /data/cache.db /opt/SickRage/cache.db
+fi
 
+if [ ! -f /data/config.ini ]
+then
 	touch /data/config.ini
 	ln -s /data/config.ini /opt/SickRage/config.ini
+fi
 
+if [ ! -f /data/failed.db ]
+then
 	touch /data/failed.db
 	ln -s /data/failed.db /opt/SickRage/failed.db
+fi
 
-	touch /data/sickbeard.db
-	ln -s /data/sickbeard.db /opt/SickRage/sickbeard.db
-
+if [ ! -d /data/cache ]
+then
 	mkdir /data/cache
 	ln -s /data/cache /opt/SickRage/cache
+fi
 
+if [ ! -d /data/Logs ]
+then
 	mkdir /data/Logs
 	ln -s /data/Logs /opt/SickRage/Logs
 fi
